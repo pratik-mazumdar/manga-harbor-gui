24a5c4aaec5eb10828a47556b7f72e10fbfacc01
Pratik <pratikmazumdar@quantiply.tech~>
2023-03-28 00:54:03 +0530

    sample  frontend

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        8: "repeat(8, minmax(0, 1fr))",
+
+        // Complex site-specific row configuration
+        layout: "200px minmax(900px, 1fr) 100px",
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ variables.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+module.exports = {
+  staticUrl: process.env.STATICURL,
+  baseUrl: process.env.BASEURL,
+  title: "Mayank chutiya h",
+};
